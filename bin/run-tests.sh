e7f50a6b04b59c706f60bff5f09677086fd54f37
0xNeshi <xinef.it@gmail.com>
2025-12-06 15:04:11 +0100

    Revert "exit on first failure"
    This reverts commit 632a2305c6684658a1dd3a2d8097787eba4adcbc.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -11,7 +11,18 @@
 # Example:
 # ./bin/run-tests.sh
 
+exit_code=0
 # Copy the tests dir to a temp dir as per docs recommendation, see:
+# https://exercism.org/docs/building/tooling/test-runners/interface
+tmp_dir='/tmp/exercism-cairo-test-runner'
+rm -rf "${tmp_dir}"
+mkdir -p "${tmp_dir}"
+cp -r tests/* "${tmp_dir}"
+
+# align scarb version when running the script locally
+[ -f .tool-versions ] && cp .tool-versions "${tmp_dir}"
+
+# Copy the tests dir to a temp dir, as per docs recommendation, see:
 # https://exercism.org/docs/building/tooling/test-runners/interface
 tmp_dir='/tmp/exercism-cairo-test-runner'
 mkdir -p "${tmp_dir}"
@@ -40,6 +51,11 @@ for test_dir in "${tmp_dir}"/*; do
     echo "$test_dir_name: comparing $(basename "${results_file_path}") to $(basename "${expected_results_file_path}")"
 
     if ! diff "$results_file_path" "$expected_results_file_path"; then
-        exit 1
+        exit_code=1
+    else
+        echo "$test_dir_name: results match"
     fi
 done
+
+rm -rf "${tmp_dir}"
+exit ${exit_code}
